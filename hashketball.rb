d0f31fbde0fb0f41ac92d0382d0228d1529a497e
victorious-messaging-4309 <wildayk@gmail.com>
2019-03-17 20:43:00 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -144,7 +144,12 @@ end
 
 def team_colors(name)
   game_hash.each do |team_name, team_data|
-
+    if team == team_data[:team_name]
+      return team_data[:colors]
+    end
+  end
+end
+   
 def team_names(name)
   game.hash.each do |team_name, team_data|
     if team == team_data[:team_name]
